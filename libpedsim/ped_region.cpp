96b57fbe24afd0096ea8366fe8ac0f918037b896
Hwasser <victorhwasser@gmail.com>
2023-02-15 12:26:01 +0100

    asaea

---
+++ libpedsim/ped_region.cpp
@@ -0,0 +1,21 @@
+#include "ped_region.h"
+
+Ped::region::region()
+{
+
+}
+
+bool Ped::region::update_position(Ped::Tagent agent, int x, int y)
+{
+
+}
+
+bool Ped::region::add(Ped::Tagent agent, int x, int y)
+{
+
+}
+
+bool Ped::region::remove(Ped::Tagent agent)
+{
+
+}
